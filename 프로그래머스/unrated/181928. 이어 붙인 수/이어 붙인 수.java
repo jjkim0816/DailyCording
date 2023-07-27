8ef1107d69b94aa6d360d24fdfda2a9f44a97047
KIM JEONG JIN <jjkim0816@gmail.com>
2023-07-27 17:47:56 +0900

    [unrated] Title: 이어 붙인 수, Time: 1.79 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        String oddNumberStr = "";
+		String evenNumberStr = "";
+        
+        for (int i = 0; i < num_list.length; i++) {
+			if (num_list[i] % 2 != 0) {
+				oddNumberStr += Integer.toString(num_list[i]);
+			} else {
+				evenNumberStr += Integer.toString(num_list[i]);
+			}
+        }
+        
+        answer = Integer.parseInt(evenNumberStr) + Integer.parseInt(oddNumberStr);
+        
+        return answer;
+    }
+}
