b7df834aa73db11ce4ff8e48b8d396bad824a2ab
KIM JEONG JIN <jjkim0816@gmail.com>
2023-07-29 12:25:22 +0900

    [unrated] Title: 부분 문자열 이어 붙여 문자열 만들기, Time: 1.51 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.java
+++ 프로그래머스/unrated/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.java
@@ -3,7 +3,7 @@ class Solution {
         String answer = "";
         
   		for (int i = 0; i < my_strings.length; i++) {
-			answer += my_strings[i].substring(parts[i][0], parts[i][1] + 1).toString();
+			answer += my_strings[i].substring(parts[i][0], parts[i][1] + 1);
 		}
         
         return answer;
