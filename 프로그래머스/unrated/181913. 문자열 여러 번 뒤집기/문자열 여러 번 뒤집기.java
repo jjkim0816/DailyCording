5c0601ce5a93d5f626256e57cd3b7c629f179829
KIM JEONG JIN <jjkim0816@gmail.com>
2023-07-29 12:05:33 +0900

    [unrated] Title: 문자열 여러 번 뒤집기, Time: 2.47 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
+++ 프로그래머스/unrated/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
@@ -1,18 +1,15 @@
 class Solution {
     public String solution(String my_string, int[][] queries) {
-        String answer = "";		char[] result = my_string.toCharArray();
-
+        String answer = "";		
+		StringBuilder resultSb = new StringBuilder(my_string);
 		for (int[] query : queries) {
-			char[] temp = new char[query[1] - query[0] + 1];
-			for (int i = query[0], j = 0; i <= query[1]; i++, j++) {
-				temp[j] = result[i];
-			}
-			for (int i = 0, j = query[1]; i < temp.length; i++, j--) {
-				result[j] = temp[i];
-			}
+			StringBuilder sb = new StringBuilder(resultSb.substring(query[0], query[1] + 1));
+			sb.reverse();
+			resultSb.replace(query[0], query[1] + 1, sb.toString());
 		}
 
-		answer = String.valueOf(result);
+		answer = resultSb.toString();
+        
         return answer;
     }
 }
