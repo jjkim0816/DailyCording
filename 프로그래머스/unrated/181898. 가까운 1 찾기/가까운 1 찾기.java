644a582666503c54adf7971e936643568e67f04e
KIM JEONG JIN <jjkim0816@gmail.com>
2023-07-29 12:46:22 +0900

    [unrated] Title: 가까운 1 찾기, Time: 0.02 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.java
+++ 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.java
@@ -2,9 +2,11 @@ class Solution {
     public int solution(int[] arr, int idx) {
         int answer = -1;
         
-        for (int i = 0; i < arr.length; i++) {
-			if (i >= idx && arr[i] == 1)
-				return i;
+        for (int i = idx; i < arr.length; i++) {
+			if (arr[i] == 1) {
+                answer = i;
+                break;
+            }
 		}
 
 		return answer;
