cc4012fdc617847e956c280206196848de471aff
KIM JEONG JIN <jjkim0816@gmail.com>
2023-07-29 13:56:42 +0900

    [unrated] Title: 순서 바꾸기, Time: 2.85 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.java
+++ 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.java
@@ -5,10 +5,10 @@ class Solution {
     public int[] solution(int[] num_list, int n) {
         int[] answer = {};
         
-        int[] first = IntStream.range(n, num_list.length).map(idx -> num_list[idx]).toArray();
-        int[] second = IntStream.range(0,n).map(idx -> num_list[idx]).toArray();
+        int[] firstArray = IntStream.range(n, num_list.length).map(idx -> num_list[idx]).toArray();
+        int[] secondArray = IntStream.range(0,n).map(idx -> num_list[idx]).toArray();
         
-        answer = IntStream.concat(IntStream.of(first), IntStream.of(second)).toArray();
+        answer = IntStream.concat(IntStream.of(firstArray), IntStream.of(secondArray)).toArray();
         
         return answer;
     }
