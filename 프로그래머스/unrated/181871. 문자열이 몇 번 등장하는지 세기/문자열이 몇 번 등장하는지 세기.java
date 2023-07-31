9a88996928bbf8eff0334e670bbeeaae39d6a79a
KIM JEONG JIN <jjkim0816@gmail.com>
2023-07-31 13:37:44 +0900

    [unrated] Title: 문자열이 몇 번 등장하는지 세기, Time: 0.08 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.java
+++ 프로그래머스/unrated/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.java
@@ -2,11 +2,10 @@ class Solution {
     public int solution(String myString, String pat) {
         int answer = 0;
         
-		for (int i = pat.length() - 1, j = 0; i < myString.length(); i++, j++) {
-			if (myString.substring(j, j + pat.length()).equals(pat)) {
-				answer += 1;
-			}
-		}
+        for (int i = 0; i < myString.length(); i++) {
+            if (myString.substring(i).startsWith(pat))
+                answer += 1;
+        }
 
         return answer;
     }
