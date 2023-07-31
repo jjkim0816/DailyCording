294ffc8507d4287b9ff498a282552352809f15d6
KIM JEONG JIN <jjkim0816@gmail.com>
2023-07-31 09:50:29 +0900

    [unrated] Title: 할 일 목록, Time: 3.00 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.java
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.java
@@ -1,20 +1,9 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.stream.IntStream;
 
 class Solution {
     public String[] solution(String[] todo_list, boolean[] finished) {
-        String[] answer = {};
-		List<String> list = new ArrayList<String>();
-
-		int[] notFinishedArray = IntStream.range(0, todo_list.length).filter(idx -> finished[idx] != true).toArray();
-		for (int i = 0; i < notFinishedArray.length; i++) {
-			list.add(todo_list[notFinishedArray[i]]);
-		}
-
-		answer = list.stream().toArray(String[]::new);
-
+        String[] answer = IntStream.range(0, todo_list.length).filter(idx -> finished[idx] != true).mapToObj(notFinishIdx -> todo_list[notFinishIdx]).toArray(String[]::new);
+	
         return answer;
     }
 }
