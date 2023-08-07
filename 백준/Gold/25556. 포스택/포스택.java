3110e1a0471c2f877073140c51c9181e43c7cf5a
KIM JEONG JIN <jjkim0816@gmail.com>
2023-08-08 08:20:15 +0900

    Update 포스택.java
    로직 재구현

--- 백준/Gold/25556. 포스택/포스택.java
+++ 백준/Gold/25556. 포스택/포스택.java
@@ -1,39 +1,40 @@
-import java.io.*;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Stack;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
-        //비교를 위한 가장 작은 수를 각 스택에 삽입한다.
-        Stack<Integer>[] stacks = new Stack[4];
-        for(int i=0; i<4; i++) {
-            stacks[i] = new Stack<>();
-            stacks[i].push(0);
-        }
+	public static void main(String[] args) {
 
-        //스택의 peek보다 큰 수라면 삽입하기
-        for(int i=0; i<n; i++) {
-            boolean flag = false;
-            for(int j=0; j<4; j++) {
-                if(stacks[j].peek() < arr[i]) {
-                    stacks[j].push(arr[i]);
-                    flag = true;
-                    break;
-                }
-            }
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		sc.nextLine();
+		int[] nStr = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+		sc.close();
 
-            //모든 스택에 삽입하지 못 했다면
-            if(!flag) {
-                System.out.println("NO");
-                return;
-            }
-        }
+		Stack<Integer>[] stack = new Stack[4];
+		boolean isCheck = false;
 
-        //순열 청소가 완료되면
-        System.out.println("YES");
+		for (int i = 0; i < stack.length; i++) {
+			stack[i] = new Stack<Integer>();
+			stack[i].push(0);
+		}
 
-    }
+		for (int i = 0; i < n; i++) {
+			isCheck = false;
+			for (int j = 0; j < stack.length; j++) {
+				if (stack[j].peek() < nStr[i]) {
+					stack[j].push(nStr[i]);
+					isCheck = true;
+					break;
+				}
+			}
+		}
+
+		if (isCheck) {
+			System.out.println("YES");
+		} else {
+			System.out.println("NO");
+		}
+	}
 }
